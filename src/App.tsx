e69ed27fb3b0923a526ceea4b3ffe2b90ecce4dd
Anh Chau <anh.chau@sensand.com>
2024-10-23 13:54:54 +1100

    added rendering based on window size

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import {
   Typography,
   Slide,
   TextField,
+  Tabs,
+  Tab,
+  useMediaQuery,
 } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
@@ -21,6 +24,12 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 interface ListItem {
   id: number,
   content: string
+}
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
 }
 
 export default function App() {
@@ -66,116 +75,17 @@ export default function App() {
         flexDirection: "column",
         flex: 1
       }}>
-        <Box sx={{ display: "flex", alignItems: "center" }}>
-          <IconButton 
-            aria-label="linkedin" 
-            onClick={() => {
-              window.open("https://www.linkedin.com/in/anhc/", '_blank');
-            }}
-            >
-            <LinkedInIcon color="primary"/>
-          </IconButton>
-          <IconButton 
-            aria-label="github" 
-            sx={{ marginLeft: 2 }}
-            onClick={() => {
-              window.open("https://github.com/tuananhc", '_blank');
-            }}
-          >
-            <GitHubIcon color="primary"/>
-          </IconButton>
-          <IconButton 
-            aria-label="instagram" 
-            sx={{ marginLeft: 2, marginRight: 4 }}
-            onClick={() => {
-              window.open("https://www.instagram.com/anh_chaau/", '_blank');
-            }}
-          >
-            <InstagramIcon color="primary"/>
-          </IconButton>
-          <Typography variant="body2" color="primary">
-            Created by Anh Cháu out of boredom and spite :D
-          </Typography>
-        </Box>
+        <HeaderAndContact setModalOpen={setModalOpen}/>
 
-        <Box sx={{ display: "flex", alignItems: "center", marginBottom: 4 }}>
-          <Typography variant="h2">
-            Anh Cháu's grading tool lmao
-          </Typography>
-          <IconButton 
-            aria-label="info" 
-            sx={{ marginLeft: 2 }}
-            onClick={() => {
-              setModalOpen(true)
-            }}
-          >
-            <InfoOutlinedIcon color="primary"/>
-          </IconButton>
-        </Box>
-
-        <Box sx={{
-          display: "flex",
-          maxWidth: 1500
-        }}>
-          <Box sx={{ display: "flex", flexDirection: "column", flex: 0.5 }}>
-            <Box sx={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              marginBottom: 5
-            }}>
-              <Typography variant="h5">
-                Add new item
-              </Typography>
-              <IconButton 
-                aria-label="add" 
-                size="large" 
-                sx={{ marginLeft: 2}}
-                onClick={addItem}
-              >
-                <AddIcon color="primary"/>
-              </IconButton>
-            </Box>
-            {data.map((item, i) => {
-              return <Item 
-              key={i}
-              id={item.id} 
-              item={item.content} 
-              copyPopUpAlert={popUpCopyAlert}
-              deletePopUpAlert={popUpDeleteAlert}
-              onChange={handleItemContentChange}
-              onRemove={removeItem}
-              />
-            })}
-          </Box>
-
-          <Box sx={{ 
-            display: "flex", 
-            flexDirection: "column", 
-            flex: 0.5, 
-            paddingLeft: 6,
-            paddingTop: 4,
-          }}>
-            <TextField 
-              label="Notes"
-              fullWidth
-              multiline
-              focused
-              variant="filled"
-              sx={{ 
-                borderRadius: 2,
-                "& .MuiInputBase-input": {
-                  color: 'blue', fontFamily: "monospace", whiteSpace: "pre", paddingTop: 2
-                },
-                "& .MuiFormLabel-root": {
-                  color: 'blue', fontFamily: "monospace", whiteSpace: "pre", fontSize: 20
-                }
-              }}
-              rows={20}
-            />
-          </Box>
-        </Box>
-
+        <ComponentsRenderedOnWindowSize 
+          data={data}
+          addItem={addItem}
+          popUpCopyAlert={popUpCopyAlert}
+          popUpDeleteAlert={popUpDeleteAlert}
+          onChange={handleItemContentChange}
+          removeItem={removeItem}
+        />
+        
         <Modal
           open={modalOpen}
           onClose={() => setModalOpen(false)}
@@ -304,4 +214,206 @@ function Item(props: any) {
     </Box>
 
   </Box>
+}
+
+function HeaderAndContact(props: any) {
+  return (
+    <Box>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <IconButton 
+          aria-label="linkedin" 
+          onClick={() => {
+            window.open("https://www.linkedin.com/in/anhc/", '_blank');
+          }}
+          >
+          <LinkedInIcon color="primary"/>
+        </IconButton>
+        <IconButton 
+          aria-label="github" 
+          sx={{ marginLeft: 2 }}
+          onClick={() => {
+            window.open("https://github.com/tuananhc", '_blank');
+          }}
+        >
+          <GitHubIcon color="primary"/>
+        </IconButton>
+        <IconButton 
+          aria-label="instagram" 
+          sx={{ marginLeft: 2, marginRight: 4 }}
+          onClick={() => {
+            window.open("https://www.instagram.com/anh_chaau/", '_blank');
+          }}
+        >
+          <InstagramIcon color="primary"/>
+        </IconButton>
+        <Typography variant="body2" color="primary">
+          Created by Anh Cháu out of boredom and spite :D
+        </Typography>
+      </Box>
+      <Box sx={{ display: "flex", alignItems: "center", marginBottom: 4 }}>
+      <Typography variant="h2">
+        Anh Cháu's grading tool lmao
+      </Typography>
+      <IconButton 
+        aria-label="info" 
+        sx={{ marginLeft: 2 }}
+        onClick={() => {
+          props.setModalOpen(true)
+        }}
+      >
+        <InfoOutlinedIcon color="primary"/>
+      </IconButton>
+    </Box>
+  </Box>
+
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+function ComponentsRenderedOnWindowSize(props: any) {
+  const matches = useMediaQuery('(min-width:800px)');
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  if (matches) {
+    return (
+      <Box sx={{
+        display: "flex",
+        maxWidth: 1500
+      }}>
+        <ItemList 
+          data={props.data}
+          addItem={props.addItem}
+          popUpCopyAlert={props.popUpCopyAlert}
+          popUpDeleteAlert={props.popUpDeleteAlert}
+          onChange={props.handleItemContentChange}
+          removeItem={props.removeItem}
+        />
+        <NoteBox />
+      </Box>
+    )
+  } else {
+    return (
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs 
+            value={value} 
+            onChange={handleChange} 
+            aria-label="basic tabs example" 
+            variant="fullWidth"
+          >
+            <Tab label="Items" {...a11yProps(0)} />
+            <Tab label="Notes" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <CustomTabPanel value={value} index={0}>
+          <ItemList 
+            data={props.data}
+            addItem={props.addItem}
+            popUpCopyAlert={props.popUpCopyAlert}
+            popUpDeleteAlert={props.popUpDeleteAlert}
+            onChange={props.onChange}
+            removeItem={props.removeItem}
+          />
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          <NoteBox />
+        </CustomTabPanel>
+      </Box>
+    );
+  }
+
+}
+
+function ItemList(props: any) {
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", flex: 0.5 }}>
+      <Box sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 5
+      }}>
+        <Typography variant="h5">
+          Add new item
+        </Typography>
+        <IconButton 
+          aria-label="add" 
+          size="large" 
+          sx={{ marginLeft: 2}}
+          onClick={props.addItem}
+          >
+          <AddIcon color="primary"/>
+        </IconButton>
+      </Box>
+      <>
+        {props.data.map((item: any, i: number) => {
+          return <Item 
+            key={i}
+            id={item.id} 
+            item={item.content} 
+            copyPopUpAlert={props.popUpCopyAlert}
+            deletePopUpAlert={props.popUpDeleteAlert}
+            onChange={props.onChange}
+            onRemove={props.removeItem}
+          />
+        })}
+      </>
+    </Box>
+  )
+}
+
+function NoteBox(props: any) {
+  return (
+    <Box sx={{ 
+      display: "flex", 
+      flexDirection: "column", 
+      flex: 0.5, 
+      paddingLeft: 6,
+      paddingTop: 4,
+    }}>
+      <TextField 
+        label="Notes"
+        fullWidth
+        multiline
+        focused
+        variant="filled"
+        sx={{ 
+          borderRadius: 2,
+          "& .MuiInputBase-input": {
+            color: 'blue', fontFamily: "monospace", whiteSpace: "pre", paddingTop: 2
+          },
+          "& .MuiFormLabel-root": {
+            color: 'blue', fontFamily: "monospace", whiteSpace: "pre", fontSize: 20
+          }
+        }}
+        rows={20}
+      />
+    </Box>
+  )
 }
